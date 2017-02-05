5efae3f30f32eb1070a8104481fc042a70610605
Tristan Arthur <RGSStudios@outlook.com>
2017-02-05 15:33:29 +1000

    Updated for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pysole',
+  packages = ['pysole'], # this must be the same as the name above
+  version = '0.1',
+  description = 'pysole is a wrapper for simulating C# console applications on any operating system that supports python.',
+  author = 'Tristan Arthur',
+  author_email = 'RGSStudios@outlook.com',
+  url = 'https://github.com/TreeStain/pysole', # use the URL to the github repo
+  download_url = 'https://github.com/TreeStain/pysole/tarball/0.1', # I'll explain this in a second
+  keywords = ['c-sharp', 'color', 'display', 'dynamic', 'linux', 'mac', 'pygame', 'pysole', 'pyterm', 'windows', 'wrapper'], # arbitrary keywords
+  classifiers = [],
+)
